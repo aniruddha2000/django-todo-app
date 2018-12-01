eb210f8c85cf24b7a4cfb26a7def5cc724f78b4a
aniruddha2000 <codewithaniruddha@gmail.com>
2018-12-02 00:47:23 +0530

    adding functionality

--- TodoApp/urls.py
+++ TodoApp/urls.py
@@ -6,5 +6,6 @@ app_name = 'TodoApp'
 urlpatterns = [
     path("add", views.add_todo, name="AddTodo"),
     path("todo", views.todo_view, name="todo"),
-    path("delete/<int:todo_id>", views.delete_todo, name="DeleteTodo")
+    path("delete/<int:todo_id>", views.delete_todo, name="DeleteTodo"),
+    path("edit/<int:todo_id>", views.edit_todo, name="EditTodo"),
 ]
--- TodoApp/views.py
+++ TodoApp/views.py
@@ -7,6 +7,9 @@ from .models import *
 
 @login_required(login_url='accounts:login')
 def todo_view(request):
+    """
+        This function for showing all todos
+    """
     context = {
         'todo_list': TodoList.objects.filter(user=request.user),
     }
@@ -15,12 +18,34 @@ def todo_view(request):
 
 @login_required(login_url='accounts:login')
 def add_todo(request):
+    """
+        This function for adding todo 
+    """
     new_item = TodoList(content=request.POST.get('content'), user=request.user)
     new_item.save()
     return HttpResponseRedirect(reverse("TodoApp:todo"))
 
 @login_required(login_url='accounts:login')
 def delete_todo(request, todo_id):
+    """
+        This function for deleting todo
+    """
     todo_delete = TodoList.objects.get(id=todo_id)
     todo_delete.delete()
     return HttpResponseRedirect(reverse("TodoApp:todo"))
+
+@login_required(login_url='accounts:login')
+def edit_todo(request, todo_id):
+    """
+        This function for edditing todo
+    """
+    todo_edit = TodoList.objects.get(id=todo_id)
+    context = {
+        'todo': todo_edit,
+    }
+    if request.method == 'POST':
+        content = request.POST.get('content')
+        todo_edit.content = content
+        todo_edit.save()
+    else:
+        return render(request, 'TodoApp/edit_todo.html', context)
