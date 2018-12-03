3dcf2d60656e748ff715ebf3dbcaedbfd75ed5a1
aniruddha2000 <codewithaniruddha@gmail.com>
2018-12-03 23:34:04 +0530

    succesfully added functionality of edditing todos

--- TodoApp/forms.py
+++ TodoApp/forms.py
@@ -19,3 +19,23 @@ class TodoForm(forms.ModelForm):
     def clean(self, *args, **kwargs):
         todo = self.cleaned_data.get('todo')
         return super(TodoForm, self).clean(*args, **kwargs)
+
+
+class EditTodoForm(forms.ModelForm):
+    todo = forms.CharField(label='', widget=forms.TextInput(
+        attrs={
+            'class': 'form-control',
+            'placeholder': 'Edit your Todos...',
+            'style': 'border-radius: 4px;',
+        }
+    ), )
+
+    class Meta:
+        model = TodoList
+        fields = [
+            'todo',
+        ]
+
+    def clean(self, *args, **kwargs):
+        todo = self.cleaned_data.get('todo')
+        return super(EditTodoForm, self).clean(*args, **kwargs)
--- TodoApp/views.py
+++ TodoApp/views.py
@@ -1,29 +1,18 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import (
+    render, 
+    redirect, 
+    get_object_or_404,
+)
 from django.contrib.auth.decorators import login_required
-from django.urls import reverse
 from .models import *
 from .forms import *
 
-
-# @login_required(login_url='accounts:login')
-# def todo_view(request):
-#     """
-#         This function for showing all todos
-#     """
-#     context = {
-#         'todo_list': TodoList.objects.filter(user=request.user),
-#     }
-
-#     return render(request, "TodoApp/todo.html", context)
 
 @login_required(login_url='accounts:login')
 def todo_view(request):
     """
         This function for adding todo 
     """
-    # new_item = TodoList(content=request.POST.get('content'), user=request.user)
-    # new_item.save()
-    # return HttpResponseRedirect(reverse("TodoApp:todo"))
     form = TodoForm(request.POST or None)
     if form.is_valid():
         new_todo = TodoList(content=form.cleaned_data.get('todo'), user=request.user)
@@ -51,12 +40,16 @@ def edit_todo(request, todo_id):
         This function for edditing todo
     """
     todo_edit = TodoList.objects.get(id=todo_id)
+    form = TodoForm(request.POST or None, instance=todo_edit)
+    if form.is_valid():
+        item = form.save(commit=False)
+        content = form.cleaned_data.get('todo')
+        item.content = content
+        item.save()
+        return redirect('TodoApp:todo')
+
     context = {
-        'todo': todo_edit,
+        'form': form,
+        'item': todo_edit,
     }
-    if request.method == 'POST':
-        content = request.POST.get('content')
-        todo_edit.content = content
-        todo_edit.save()
-    else:
-        return render(request, 'TodoApp/edit_todo.html', context)
+    return render(request, "TodoApp/edit_todo.html", context)
